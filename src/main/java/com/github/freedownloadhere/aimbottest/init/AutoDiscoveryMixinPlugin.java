223428e98d41413211f6913e2b8e23bfe660890b
GitHub Action <action@github.com>
2024-04-21 13:12:28 +0000

    Initialize template repository proper
    Set package name to com.github.freedownloadhere.aimbottest
    Set project name to Aimbot-Test
    Set modid to aimbottest

--- src/main/java/com/github/freedownloadhere/aimbottest/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/freedownloadhere/aimbottest/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.freedownloadhere.aimbottest.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/freedownloadhere/aimbottest/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/freedownloadhere/aimbottest/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.freedownloadhere.aimbottest.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
